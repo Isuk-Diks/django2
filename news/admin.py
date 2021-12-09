b1f77c132cbaf5dce5e2bb9ad28c57494d06a7dc
Isuk-Diks <jhonybone@yandex.ru>
2021-12-09 22:41:09 +0500

    models added

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Project, Article, Comment, Message
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+class ArticleModelAdmin(SummernoteModelAdmin):  
+    summernote_fields = '__all__'
+
+admin.site.register(Project)
+admin.site.register(Article, ArticleModelAdmin)
+admin.site.register(Comment)
+admin.site.register(Message)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+	photo = models.ImageField(upload_to="media")
+	title = models.CharField(max_length=200)
+	description = models.TextField()
+	created = models.DateTimeField(auto_now=True)
+	slug = models.SlugField(max_length=255)
+	def __str__(self):
+		return self.name
+
+class Article(models.Model):
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created = models.DateTimeField(auto_now=True)
+	slug = models.SlugField(max_length=255)
+	def __str__(self):
+		return self.title
+
+class Comment(models.Model):
+	text = models.CharField(max_length=300)
+	author = models.CharField(max_length=100)
+	author_position = models.CharField(max_length=100)
+
+class Message(models.Model):
+	name = models.CharField(max_length=200)
+	subject = models.CharField(max_length=200)
+	email = models.EmailField()
+	text = models.TextField()
+	def __str__(self):
+		return f"{self.name} - {self.subject}"
