ae8b1851b7a71dabda08409113babbfcecab236c
Isuk-Diks <jhonybone@yandex.ru>
2021-12-18 22:19:15 +0500

    going to the next and previous page added

--- news/views.py
+++ news/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.db.models import OuterRef, Subquery
+from django.db import models
 from .models import Article, Mention, Project
+
 
 def blog_handler(request):
     articles = Article.objects.all()[:7]
@@ -9,8 +12,16 @@ def blog_handler(request):
 
 
 def detail_post(request, slug):
-    article = get_object_or_404(Article.objects.prefetch_related('comments'), slug=slug)
-    context = {'article':article}
+    next_article = Article.objects.filter(created__gte=OuterRef('created')).exclude(id=OuterRef('id'))[:1]
+    previous_article = Article.objects.filter(created__lte=OuterRef('created')).exclude(id=OuterRef('id'))[:1]
+    article = get_object_or_404(Article.objects.prefetch_related('comments').annotate(
+            next_article=Subquery(next_article.values("title")[:1]),
+            next_article_slug=Subquery(next_article.values("slug")[:1]),
+            previous_article=Subquery(previous_article.values("title")[:1]),
+            previous_article_slug=Subquery(previous_article.values("slug")[:1]),),
+            slug=slug)
+    context = {
+            'article':article}
     return render(request, 'news/blog-single.html', context)
 
 
