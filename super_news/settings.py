bb3c6a8d834a0a762de1e4e5b51343c365b3dcc4
Isuk-Diks <jhonybone@yandex.ru>
2021-12-25 13:20:20 +0500

    settings changed

--- super_news/settings.py
+++ super_news/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i%c^d9*r3l*sk=5w@t&$*k+m1o#s-vla*m+5^%rv(k=#c-e*0m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['165.232.72.158']
 
 INTERNAL_IPS = [
     "127.0.0.1",
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'super_news.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'USER':'admin',
+        'PASSWORD':'password',
+        'HOST':'127.0.0.1',
+        'PORT':5432
     }
 }
 
